cd008c66319e8f2035d0ffa79904decd46ef1f7e
Juan Tavarez <tavarezju@fondesa.org>
2020-06-26 19:55:39 -0400

    Pages and components working

--- src/components/TableContainer.svelte
+++ src/components/TableContainer.svelte
@@ -1,6 +1,8 @@
 <script>
-import CovidStat from 'CovidStat';
-import Table
+import TableFilter from './TableFilter.svelte';
+import Table from './Table.svelte';
 </script>
 
 <h1>Table Container</h1>
+<TableFilter/>
+<Table/>
