d2d0e3c072609ed65b1a93fd9b3b7b404ca6442d
Juan Tavarez <tavarezju@fondesa.org>
2020-08-11 19:57:11 -0400

    ports

--- src/components/TableFilter.svelte
+++ src/components/TableFilter.svelte
@@ -16,7 +16,7 @@ input, .select, select {
 <div class="container">
 <div class="columns">
 <div class="column is-6">
-<input type="text" class="input" bind:value={stateName} placeholder="Filter States">
+<input type="text" class="input" bind:value={stateName} placeholder="Filter Per States">
 </div>
 <div class="column is-6">
 <div class="select">
